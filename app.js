139175dcfa515298840912c097478f4cd9587128
FKeir <fmkeir@gmail.com>
2020-02-22 10:52:24 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.querySelector('#dragonForm');
+  const submit = document.querySelector('#submit');
+  const list = document.querySelector('#dragonList');
+});
