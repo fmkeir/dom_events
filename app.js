ff292d69089bcab5cdc0a4613fca0a60132d4d6d
FKeir <fmkeir@gmail.com>
2020-02-22 11:02:20 +0000

    Add submit event listener and create new list item with inputted details

--- app.js
+++ app.js
@@ -2,4 +2,23 @@ document.addEventListener('DOMContentLoaded', () => {
   const form = document.querySelector('#dragonForm');
   const submit = document.querySelector('#submit');
   const list = document.querySelector('#dragonList');
+
+  submit.addEventListener('click', () => {
+    handleButtonClick(form, list);
+  });
 });
+
+const handleButtonClick = (input, list) => {
+  event.preventDefault();
+  const newListItem = document.createElement('li');
+  const name = document.createElement('h2');
+  const colour = document.createElement('h3');
+  const size = document.createElement('h4');
+  name.textContent = `${input.name.value}`;
+  colour.textContent = `${input.colour.value}`;
+  size.textContent = `${input.size.value}`;
+  newListItem.appendChild(name);
+  newListItem.appendChild(colour);
+  newListItem.appendChild(size);
+  list.appendChild(newListItem);
+}
